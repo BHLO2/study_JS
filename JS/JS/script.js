3d6ff31d38512e703e881f29625750d974829847
Vladimir Rasyuk <sdmet6d@rambler.ru>
2019-06-29 21:44:23 +0300

    commit4

---
+++ JS/JS/script.js
@@ -0,0 +1,24 @@
+let money = 50000;
+let income = 'freelance, coven';
+let addExpenses = 'Quart Board, Travel, Study';
+let deposit = true;
+let mission = 100000;
+let period = 5;
+
+
+
+console.log(money, income, deposit);
+console.log(income.length);
+console.log(period, mission);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+let budgetDay = (money / 30);
+console.log(budgetDay);
+
+
+
+
+
+
+
---
+++ JScomplex/JS/script.js
@@ -0,0 +1,15 @@
+let num = 266219;
+console.log(2 * 6 * 6 * 2 * 1 * 9);
+
+
+console.log(1296**3);
+let num2 = 2176782336;
+
+let digit = num2.toString();
+console.log(digit.substring(0,2)); //Вывелk в коносль первые две цифры
+
+
+
+
+
+
